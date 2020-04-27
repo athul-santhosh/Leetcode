febd8d528ba36035592908412d4918020fcad769
athul <42475189+thenex14@users.noreply.github.com>
2020-04-27 10:21:48 +0300

    Update Reverse a Integer.py

--- Reverse a Integer.py
+++ Reverse a Integer.py
@@ -7,7 +7,7 @@ class Solution:
         while x != 0:                         #is negative 
             rev = rev*10 + x % 10             # rev*10 cancels out 0s and
             x = x//10                         # aswell as increments 10s
-        if -2**31 < rev < 2**31 -1:           # checking the range of revv
+        if -2**31 < rev < 2**31 -1:           # checking the range of revv wehter is in the int range
             if temp > 0:                          
                 return rev                         
             else:
